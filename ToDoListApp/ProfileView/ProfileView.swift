8e1851c75bbf444f51229ba6ce0c73601647a729
Sacfffff <merzoyan.sasha@yandex.ru>
2023-05-13 22:10:55 +0300

    create profile view

--- ToDoListApp/ProfileView/ProfileView.swift
+++ ToDoListApp/ProfileView/ProfileView.swift
@@ -14,11 +14,61 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
-                
+                if let user = viewModel.user {
+                    profile(user: user)
+                } else {
+                    Text("Loading profile")
+                }
+
             }
             .navigationTitle("Profile")
         }
+        .onAppear {
+            viewModel.fetchUser()
+        }
     }
+    
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .foregroundColor(.blue)
+            .frame(width: 120, height: 120)
+            .padding()
+        
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Name:")
+                    .bold()
+                Text(user.name)
+            }.padding()
+            
+            HStack {
+                Text("Email:")
+                    .bold()
+                Text(user.email)
+            }
+            .padding()
+            
+            HStack {
+                Text("Member since:")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }.padding()
+            
+        }.padding()
+
+        Button {
+            viewModel.logOut()
+        } label: {
+            Text("LogOut")
+                .foregroundColor(.white)
+        }
+        .foregroundColor(.red)
+    }
+    
 }
 
 struct ProfileView_Previews: PreviewProvider {
--- ToDoListApp/ProfileView/ProfileViewViewModel.swift
+++ ToDoListApp/ProfileView/ProfileViewViewModel.swift
@@ -5,10 +5,43 @@
 //  Created by Aleks Kravtsova on 1.05.23.
 //
 
+import FirebaseFirestore
+import FirebaseAuth
 import Foundation
 
 final class ProfileViewViewModel : ObservableObject {
     
+    @Published var user : User? = nil
     init() {}
+    
+    func fetchUser() {
+        
+        guard let uuid = Auth.auth().currentUser?.uid else { return }
+        
+        let db = Firestore.firestore()
+        db.collection(FirestoreKeys.usersPath)
+            .document(uuid)
+            .getDocument { [weak self] snapshot, error in
+                if let data = snapshot?.data(), error == nil  {
+                    DispatchQueue.main.async {
+                        self?.user = User(id: data["id"] as? String ?? "", name: data["name"] as? String ?? "", email: data["email"] as? String ?? "", joined: data["joined"] as? TimeInterval ?? 0)
+                    }
+                }
+            }
+        
+    }
+    
+    func logOut() {
+        
+        do {
+           
+            try Auth.auth().signOut()
+            
+        } catch {
+            
+            print(error)
+        }
+        
+    }
     
 }
